38e8c52aa975218a05bb5ab7ce6ed999f2bb530b
Stanisław Bugalski <45152735+sbugalski@users.noreply.github.com>
2020-06-21 21:43:38 +0200

    fix alias, add kubectx and kubens funcitons

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -19,4 +19,54 @@ $DefaultUser = $env:USERNAME
 
 # Aliases
 New-Alias -Name k -Value kubectl -Force
-New-Alias -Name kalp -Value "kubectl run alpine --rm=true -it --image=alpine sh" -Force
+
+function global:Run-KubeAlpine {
+  [CmdletBinding()]
+  [Alias('kalp')]
+  [Parameter(ValueFromRemainingArguments = $true)]$params
+  & kubectl run alpine --rm=true -it --image=alpine sh $params
+}
+
+# source https://medium.com/rkttu/handy-kubernetes-context-namespace-switcher-for-powershell-a432ff8ae7cd
+function global:Select-KubeContext {
+  [CmdletBinding()]
+  [Alias('kubectx')]
+  param (
+    [parameter(Mandatory=$False,Position=0,ValueFromRemainingArguments=$True)]
+    [Object[]] $Arguments
+  )
+  begin {
+    if ($Arguments.Length -gt 0) {
+      $ctx = & kubectl config get-contexts -o=name | fzf -q @Arguments
+    } else {
+      $ctx = & kubectl config get-contexts -o=name | fzf
+    }
+  }
+  process {
+    if ($ctx -ne '') {
+      & kubectl config use-context $ctx
+    }
+  }
+}
+
+function global:Select-KubeNamespace {
+  [CmdletBinding()]
+  [Alias('kubens')]
+  param (
+    [parameter(Mandatory=$False,Position=0,ValueFromRemainingArguments=$True)]
+    [Object[]] $Arguments
+  )
+  begin {
+    if ($Arguments.Length -gt 0) {
+      $ns = & kubectl get namespace -o=name | fzf -q @Arguments
+    } else {
+      $ns = & kubectl get namespace -o=name | fzf
+    }
+  }
+  process {
+    if ($ns -ne '') {
+      $ns = $ns -replace '^namespace/'
+      & kubectl config set-context --current --namespace=$ns
+    }
+  }
+}
